0a6968e16962dcd8d709c7eadb002b9b5399ccfb
biancamarinoj <129007175+biancamarinoj@users.noreply.github.com>
2023-12-05 00:04:54 -0300

    Update tp6.js

--- tp6.js
+++ tp6.js
@@ -1,23 +1,28 @@
-let mb = []; //ARRAY DE IMAGENES ;)
+//TP Final — Etapa 1
+
 let a; //declaro un objeto de la clase AVENTURA
-let texto = [];
-let j;
+let j; //declaro un objeto de la clase JUEGO
+let mb = []; //array de IMAGENES
+let texto = []; //array de TEXTOS
+
 function setup() {
   createCanvas(600, 600);
   a = new Aventura(mb, texto);
   j = new Juego();
 }
 
-
 function draw() {
   a.dibujar(mb, texto);
 }
+
 function mousePressed() {
   a.botonPresionado();
 }
+
 function keyPressed() {
   a.pasaje();
 }
+
 function preload() {
   for (let i = 0; i < 26; i++) {
     mb[i] = loadImage('assets/mb'+ i +'.png');
