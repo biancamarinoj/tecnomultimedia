cfff5dda4f9e7ad51828c61f74eca9287449703d
biancamarinoj <129007175+biancamarinoj@users.noreply.github.com>
2023-12-06 11:13:30 -0300

    Update boton.js

--- boton.js
+++ boton.js
@@ -1,13 +1,15 @@
 class Boton {
   constructor(x, y, r) {
+    this.y= y;
+    this.x= x;
     this.tamanioaumentado=103;
     this.radio=r;
     this.botoninicio=true;
   }
-  
+
   dibujar(imgArray, m, b, estado) {
-    image(imgArray, m, b);
-    if (estado !== 6 && dist(mouseX, mouseY,530, 340)<=this.radio) { //menos en el estado 6, si posiciono el mouse en la imagen la agranda
+    image(imgArray, this.x, this.y);
+    if (estado !== 6 && dist(mouseX, mouseY, 530, 340)<=this.radio) { //menos en el estado 6, si posiciono el mouse en la imagen la agranda
       image(imgArray, 496, 292, this.tamanioaumentado, this.tamanioaumentado);
     }
   }
